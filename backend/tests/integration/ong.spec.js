31b8e1256f34b0ed2274bf8d5125a67a3b0cf577
Marcos Oliveira <ziguifrido@gmail.com>
2020-03-29 20:35:44 -0400

    Test

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
+    it('should be able to create a new ONG', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name: "OTIA",
+                email: "contato@otia.com",
+                whatsapp: "65888888888",
+                city: "Cuiabá",
+                uf: "MT"
+            });
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+    });
+});
